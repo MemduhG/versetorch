f57968ba83302ff7622713f131066c68201e8767
Memduh Gokirmak <mgokirmak@zetaglobal.com>
2020-11-13 21:00:18 +0100

    remove spacy

--- src/harvard.py
+++ src/harvard.py
@@ -428,35 +428,25 @@ def greedy_decode(model, src, src_mask, max_len, start_symbol):
                         torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
     return ys
 
-# For data loading.
-from torchtext import data, datasets
 
-if True:
-    import spacy
-    spacy_de = spacy.load('de')
-    spacy_en = spacy.load('en')
+from torchtext import data
 
-    def tokenize_de(text):
-        return [tok.text for tok in spacy_de.tokenizer(text)]
 
-    def tokenize_en(text):
-        return [tok.text for tok in spacy_en.tokenizer(text)]
-
-    # BOS_WORD = '<s>'
-    # EOS_WORD = '</s>'
-    # BLANK_WORD = "<blank>"
-    # SRC = data.Field(tokenize=tokenize_de, pad_token=BLANK_WORD)
-    # TGT = data.Field(tokenize=tokenize_en, init_token = BOS_WORD,
-    #                  eos_token = EOS_WORD, pad_token=BLANK_WORD)
-    #
-    # MAX_LEN = 100
-    # train, val, test = datasets.IWSLT.splits(
-    #     exts=('.de', '.en'), fields=(SRC, TGT),
-    #     filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and
-    #         len(vars(x)['trg']) <= MAX_LEN)
-    # MIN_FREQ = 2
-    # SRC.build_vocab(train.src, min_freq=MIN_FREQ)
-    # TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
+# BOS_WORD = '<s>'
+# EOS_WORD = '</s>'
+# BLANK_WORD = "<blank>"
+# SRC = data.Field(tokenize=tokenize_de, pad_token=BLANK_WORD)
+# TGT = data.Field(tokenize=tokenize_en, init_token = BOS_WORD,
+#                  eos_token = EOS_WORD, pad_token=BLANK_WORD)
+#
+# MAX_LEN = 100
+# train, val, test = datasets.IWSLT.splits(
+#     exts=('.de', '.en'), fields=(SRC, TGT),
+#     filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and
+#         len(vars(x)['trg']) <= MAX_LEN)
+# MIN_FREQ = 2
+# SRC.build_vocab(train.src, min_freq=MIN_FREQ)
+# TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
 
 SRC, TGT, train, val, test = get_dataset("tr")
 
