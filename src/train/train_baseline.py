fbb0a35814f13088a6bc374907f2b22f911b0634
Memduh Gokirmak <mgokirmak@zetaglobal.com>
2021-02-12 12:11:11 +0300

    try deleting tensors

--- src/train/train_baseline.py
+++ src/train/train_baseline.py
@@ -41,7 +41,7 @@ def run_epoch(data_iter, model, loss_compute, tokenizer, save_path=None, validat
         del batch.src, batch.trg, batch.src_mask, batch.trg_mask
         loss = loss_compute(out, batch.trg_y, batch.ntokens)
         del out
-        total_loss += loss
+        total_loss += float(loss)  # this might be the problem
         total_tokens += batch.ntokens
         tokens += batch.ntokens
         if loss_compute is not None:
