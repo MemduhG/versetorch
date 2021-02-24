b1be4ce9e52d41168baff911292c209aab5ddf37
Memduh Gokirmak <mgokirmak@zetaglobal.com>
2021-02-24 10:18:11 +0300

    adafactor

--- src/train/rf.py
+++ src/train/rf.py
@@ -3,10 +3,10 @@ import sys
 import os
 import time
 from reformer_pytorch import ReformerEncDec
-from transformers.optimization import Adafactor
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 
+from src.model.adafactor import Adafactor
 from src.utils.utils import get_tokenizer
 from src.model.model import NoamOpt
 from src.data_utils.data import get_training_iterators
@@ -16,21 +16,12 @@ save_every = 1800
 save_path = "checkpoints/tur-rf"
 MAX_SEQ_LEN = 512
 
-enc_dec = ReformerEncDec(dim=512, enc_num_tokens=32000, enc_depth=6, enc_max_seq_len=MAX_SEQ_LEN, dec_num_tokens=32000,
-                         dec_depth=6, dec_max_seq_len=MAX_SEQ_LEN, ignore_index=3, pad_value=3).cuda()
+device = 0 if torch.cuda.device_count() > 0 else "cpu"
 
-opt = Adafactor(
-    enc_dec.parameters(),
-    lr=1e-3,
-    eps=(1e-30, 1e-3),
-    clip_threshold=1.0,
-    decay_rate=-0.8,
-    beta1=None,
-    weight_decay=0.0,
-    relative_step=False,
-    scale_parameter=False,
-    warmup_init=False
-)
+enc_dec = ReformerEncDec(dim=512, enc_num_tokens=32000, enc_depth=6, enc_max_seq_len=MAX_SEQ_LEN, dec_num_tokens=32000,
+                         dec_depth=6, dec_max_seq_len=MAX_SEQ_LEN, ignore_index=3, pad_value=3).to(device)
+
+opt = Adafactor(enc_dec.parameters())
 
 tokenizer = get_tokenizer("tr")
 train_iter, valid_iter, test_iter, train_idx, dev_idx, test_idx = get_training_iterators("tur")
@@ -39,8 +30,10 @@ steps = load_latest(save_path, enc_dec, opt)
 
 last_saved = time.time()
 
+
+
 for batch in train_iter:
-    src, tgt = torch.transpose(batch.src, 0, 1).cuda(), torch.transpose(batch.trg, 0, 1).cuda()
+    src, tgt = torch.transpose(batch.src, 0, 1).to(device), torch.transpose(batch.trg, 0, 1).to(device)
     print(src.shape, tgt.shape)
     input_mask = src != 3
     try:
